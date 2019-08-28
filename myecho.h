e95f01bc47ceb174ef6852700f5862d58086abe5
minggongzero <minggongzero@gmail.com>
2019-08-28 16:04:09 +0800

    Head file

---
+++ myecho.h
@@ -0,0 +1,19 @@
+#ifndef MYECHO_H_INCLUDED
+#define MYECHO_H_INCLUDED
+
+void echsrv();
+void echcli();
+void p2psrv();
+void p2pcil();
+void echsrv_nb();
+void echcli_nb();
+void echsrv_readline();
+void echsrv_select();
+void echcli_select();
+void echsrv_poll();
+void echsrv_epoll();
+void echsrv_msq();
+void echcli_msq();
+void echsrv_pthread();
+
+#endif // MYECHO_H_INCLUDED
